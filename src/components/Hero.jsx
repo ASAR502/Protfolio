050402dfe8f6ea1b2a876d6753f9280eb0900d13
Aaquib <aaquibm502@gmail.com>
2024-11-22 22:10:01 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-10 fle`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-10 mb-10`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
@@ -19,16 +19,16 @@ const Hero = () => {
             Hi, I'm{" "}
             <span className="text-[#915EFF] font-thin">Aaquib Shahzada</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              FullStack Developer <br className="sm:block hidden" />
-             
+          <p className={`${styles.heroSubText} mt-2 text-white-100 `}>
+            I’m Aaquib, a software developer fascinated in Crafting Scalable,
+            Production-Ready Web Applications <br className="sm:block hidden" />
           </p>
         </div>
       </div>
 
-<div className="w-full h-[80rem]" >
-      <ComputersCanvas /> 
-      </div> 
+      <div className=" flex w-full h-[80rem] mt-4">
+        <ComputersCanvas />
+      </div>
     </section>
   );
 };
